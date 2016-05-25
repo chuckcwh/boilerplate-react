07a3776f50a7afb229cdbf3bde731759209af83a
chuckcwh <chuckcwh@gmail.com>
2016-05-25 17:03:16 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Default React (v0.1.0)
+
+My basic settings of react.
+
+# Include:
+
+1. react / react-dom
+
+2. webpack
+
+3. server.js (can be removed)
+
+# How to Use:
+
+1. npm install
+
+(updated: May25, 2016)
---
+++ app/app.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var firstName = "Chuck";
+
+ReactDOM.render(
+  <h1>Boilerplate with React and Webpack!</h1>,
+  document.getElementById('app')
+);
